5b253938c7b6e3c085502874d322030858b35064
AurelieDombre <au.dombre@gmail.com>
2026-05-08 18:27:40 +0200

    ajout du lazyload pour avoir un effet illimité dans le load + typage de useFetchQuery

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
 import { ThemedText } from "@/components/ThemedText";
 import { Link } from "expo-router";
-import { FlatList, Image, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useUserThemeColors } from "@/hooks/userThemesColors";
 import { Card } from "@/components/Card";
 import { PokemonCard } from "@/components/pokemon/PokemonCard";
-import { useFetchQuery } from "@/hooks/useFetchQuery";
+import { useFetchQuery, useInfiniteFetchQuery } from "@/hooks/useFetchQuery";
 import { getPokemonIdFromUrl } from "@/functions/pokemon";
 
 export default function Index() {
@@ -14,8 +14,18 @@ export default function Index() {
   // en fonction de l'index k du tableau généré par Array.from. Array.from({ length: 35 }) génère un tableau de 35 éléments, 
   // et (_, k) => ... est la fonction qui est appelée pour chaque élément du tableau, avec _ qui représente la valeur de l'élément (qui est undefined dans ce cas)
   // et k qui représente l'index de l'élément (de 0 à 34). En ajoutant 1 à k, on obtient des id de pokémon allant de 1 à 35.
-  const {data} = useFetchQuery('/pokemon?limit=21'); // Récupérer les données de l'api pour les pokémons, avec une limite de 21 pokémons
-  const pokemons = data?.results ?? [] // Récupérer les résultats de l'api, qui sont les pokémons, et les stocker dans une variable
+
+  // Récupérer les données de l'api pour les pokémons, avec une limite de 21 pokémons 
+/*   const {data, isFetching} = useFetchQuery('/pokemon?limit=21');  */
+// Récupérer les donnée de l'api pour les pokémons, avec une pagination infinie, en utilisant la fonction useInfiniteFetchQuery qui utilise la fonction useInfiniteQuery de react-query pour gérer la pagination, et en affichant un loader pendant le chargement des données
+  const { data, isFetching, fetchNextPage } = useInfiniteFetchQuery('/pokemon?limit=35');
+  // isFetching est une variable qui indique si les données sont en train d'être récupérées, pour afficher un loader pendant le chargement des données
+  //fetchNextPage est une fonction qui permet de récupérer la page suivante de données, en l'appelant lorsque l'on atteint la fin de la liste dans la flatlist grâce à onEndReached.
+
+  // Récupérer les résultats de l'api, qui sont les pokémons, et les stocker dans une variable
+  /* const pokemons = data?.results ?? []  => si on fait avec la limitation de résultats*/
+  // Réceptionner les pages pour avoir un effet infinite scroll, et les aplatir pour avoir un tableau de pokémons à afficher dans la flatlist
+  const pokemons = data?.pages.flatMap(page => page.results) ?? [];
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: colors.tint }]}>
@@ -31,6 +41,16 @@ export default function Index() {
                   numColumns={3}
                   columnWrapperStyle={styles.gridGap}
                   contentContainerStyle={[styles.gridGap, styles.list]}
+                  /* Affiche un petit loader pendant le chargement des datas */
+                  ListFooterComponent={
+                    isFetching ? <ActivityIndicator color={colors.tint} /> : null
+                  }
+                  /* Permet de détecter lorsqu'on atteint la fin de la liste, pour charger les pages suivantes */
+                  onEndReached={() => {
+                    if (!isFetching) {
+                      fetchNextPage();
+                    }
+                  }}
                   renderItem={({ item }) => 
             // Afficher le nom et l'id du pokémon dans une vue, en utilisant le composant ThemedText pour le style du texte
             <PokemonCard name={item.name} id={getPokemonIdFromUrl(item.url)} style={{ flex: 1/3}}  /> } keyExtractor={(item) => item.url} 
--- hooks/useFetchQuery.ts
+++ hooks/useFetchQuery.ts
@@ -1,20 +1,60 @@
 // Va permettre de récupérer dynamiquement mes noms des pokémons et leurs id, pour les afficher dans ma flatlist, en utilisant l'api pokeapi.co. 
 // Il faut installer le package : npm i @tanstack/react-query
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 const endpoint = 'https://pokeapi.co/api/v2'; // L'endpoint de l'api pour récupérer les pokémons, avec une limite de 35 pokémons
 
-export function useFetchQuery(path: string) {
+// Définir les points d'entrée de l'API. JE vais lui donner les résultats que je vais obtenir
+type API ={
+    '/pokemon?limit=21': {
+        count: number;
+        next: string | null;
+        results: {name: string, url: string}[]
+    };
+}
+
+// <T extends keyof API> signifie que la fonction useFetchQuery prend un paramètre de type T qui doit être une clé de l'interface API, c'est à dire une des routes de l'api que j'ai définie dans l'interface API, 
+// et que la fonction retourne les données correspondantes à cette route,
+//  en utilisant le type défini dans l'interface API pour cette route. Par exemple,
+//  si je passe '/pokemon?limit=21' comme argument à la fonction useFetchQuery, elle va retourner un objet avec les propriétés count, 
+// next et results, qui sont définies dans l'interface API pour la route '/pokemon?limit=21'.
+export function useFetchQuery<T extends keyof API>(path: T) {
     return useQuery({
         queryKey: [path],
         queryFn: async () => {
             await wait(1); // Simuler un délai de 1 seconde pour le chargement des données
-            return fetch(endpoint + path).then(res => res.json()); // Récupérer les données de l'api et les convertir en json, puis les retourner
-            
+            return fetch(endpoint + path,{
+                headers: {
+                    Accept: 'application/json'
+                }
+            }).then(res => res.json() as Promise<API[T]>); // Récupérer les données de l'api et les convertir en json, puis les retourner en utilisant le type défini dans l'interface API pour la route correspondante
+
         }
     });
 }
 
+export function useInfiniteFetchQuery<T extends keyof API>(path: T) {
+    return useInfiniteQuery({
+        queryKey: [path],
+        initialPageParam: endpoint + path,
+        // queryFn est une fonction qui récupère les données de l'api en fonction du paramètre pageParam, qui est l'url de la page à récupérer, et les convertit en json, puis les retourne
+        queryFn: async ({ pageParam }) => {
+            await wait(1);
+            return fetch(pageParam, {
+                headers: {
+                    Accept: 'application/json'
+                }
+            }).then(res => res.json() as Promise<API[T]>);
+        },
+        // getNextPageParam est une fonction qui récupère l'url de la page suivante à partir des données de la page actuelle, en utilisant la propriété next de l'objet retourné par l'api, et la retourne
+        getNextPageParam: (lastPage) => {
+            if ("next" in lastPage){
+                return lastPage.next;
+            }
+                return null; // Si il n'y a pas de page suivante, retourner null pour indiquer que la pagination est terminée
+        }
+    })
+}
 
 function wait(duration: number): Promise<void> {
     return new Promise((resolve) => {
